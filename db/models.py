14ea560ec69d1aa77e9711ed9f7b613155012a80
unknown <lh3241pon@gmail.com>
2026-08-27 23:05:46 -0300

    Solution 4

--- db/models.py
+++ db/models.py
@@ -1,15 +1,15 @@
+# db/models.py
 from django.db import models
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=100)
-    bonus = models.TextField()
-
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)  # Não TextField!
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
@@ -18,24 +18,25 @@ class Skill(models.Model):
 
 
 class Guild(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=100)
-    email = models.EmailField()
-    bio = models.TextField()
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)  # Não TextField!
+    created_at = models.DateTimeField(auto_now_add=True)
 
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
         related_name="players",
     )
-
     guild = models.ForeignKey(
         Guild,
         on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         related_name="players",
     )
